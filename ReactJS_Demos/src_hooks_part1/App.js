f9e5af6d139bc273a976c39447550433658a400c
= <=>
2023-02-02 12:30:43 +0530

    Hooks part1 demos and notes

---
+++ ReactJS_Demos/src_hooks_part1/App.js
@@ -0,0 +1,52 @@
+
+import { useState } from 'react';
+import './App.css';
+import UseEffectDemo from './UseEffectDemo';
+import UseReducerDemo from './UseReducerDemo';
+
+
+function App() {
+
+        const  nameVariable =          useState('javeed');
+
+        const  [city,setCity] = useState('hyderabad');
+
+
+  const updateStateFunc = ()=>{
+
+
+      nameVariable[1]('king');
+
+      setCity('Delhi');
+
+  }
+
+
+  return (
+   
+    <>
+
+ 
+      <h1>Welcome to App Component</h1>
+
+      <h1>Name: {nameVariable[0]}</h1>
+
+      <h1>City: {city}</h1>
+
+      <button onClick={updateStateFunc}>Update</button>
+
+      <h2>Use Effect Demo Below</h2>
+
+      <UseEffectDemo />
+
+      <h2>Use Reducer Demo</h2>
+
+
+      <UseReducerDemo />
+
+
+    </>
+  );
+}
+
+export default App;
